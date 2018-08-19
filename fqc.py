dd6784c75c18bc76d9e4489d73cf7b4c686b8eab
hjdong <1901919946@qq.com>
2018-08-19 11:09:19 +0800

    Update and rename fq.py to fqc.py

--- fqc.py
+++ fqc.py
@@ -1,14 +1,18 @@
+#/usr/bin/env python3
 
 import sys
 import gzip
 
 args = sys.argv
 filename = args[1]
+name = filename.split("_")[0]
 
 NUM = 0
 count = 0
 q30 = 0
-with open(filename,"r") as fq:
+gc = 0
+
+with gzip.open(filename,"rt") as fq:
 	while True:
 		line_1 = fq.readline().strip('\n')
 		if not (line_1 and fq):
@@ -16,23 +20,16 @@ with open(filename,"r") as fq:
 		line_2 = fq.readline().strip('\n')
 		line_3 = fq.readline().strip('\n')
 		line_4 = fq.readline().strip('\n')
-		NUM +=len(line_2)
-
 		for q in line_4:
 			qual = ord(q)-33
 			if qual >=30:
 				q30 +=1
-		
-		
-with open(filename,"r") as fq:
-	while True:
-		buffer = fq.read(8*1024*1024)
-		if not buffer:
-			break
-		count +=buffer.count('\n')
+		NUM +=len(line_2)
+		count += 1
+		gc += line_2.count("G")+line_2.count("C"
 
-print(filename,end='\t')
-print(int(count/4),end='\t')
+print(name,end='\t')
+print(count,end='\t')
 print(NUM/1000000,end='\t')
 print('%.2f' % (q30/NUM*100))
 
