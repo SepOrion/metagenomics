0f02589f0d8d22ef6118b16e2b5977917003619b
hjdong <1901919946@qq.com>
2018-08-22 15:59:13 +0800

    Update remove_reads_genes.py

--- remove_reads_genes.py
+++ remove_reads_genes.py
@@ -3,26 +3,28 @@
 # Usage:filter_reads_in_genes.py <genes_reads_file> <output_name>
 # 去除在各样品中支持reads数小于等于2的基因，并统计每个样品中丰度大于0的基因数
 
-import sys
+import argparse
 import pandas as pd
 import numpy as np
 
-input_file = sys.argv[1]
-output_file = sys.argv[2]
+parser = argparse.ArgumentParser(description="用途：去除在各样品中支持reads数小于等于2的基因，并统计每个样品中丰度大于0的基因数")
+parser.add_argument("-i",dest="inputFile",help="所有样品reads数矩阵表，第一列基因编号，第二列为长度，其余各列为样品名")
+parser.add_argument("-o",dest="outFile",metavar="",help="输出文件名")
+args = parser.parse_args()
 
-df = pd.read_table(input_file,index_col=[0])
+df = pd.read_table(args.inputFile,index_col=[0])
 
-# remove reads<=2 genes
+# 过滤掉在各个样品中支持 reads 数目<=2的基因
 dff = df[(np.abs(df.iloc[:,1:])>2).any(1)] 
 
 f = lambda x: x/dff['Length']
 data = dff.iloc[:,1:].apply(f)
 
-# Calculate relative abundance
+# 计算基因相对丰度
 num = data.sum()
 data1=data/num 
-data1.to_csv(output_file,sep="\t")
+data1.to_csv(args.outFile,sep="\t")
 
-# Count the number of genes in each sample
+# 统计样品丰度不为0的基因数
 counts = (dff>0).sum()
 counts.to_csv("genes_counts.txt",sep="\t")
