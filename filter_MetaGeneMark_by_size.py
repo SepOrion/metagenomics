edc7c70f572ccca73b0528fcb9dd254442d793db
hjdong <1901919946@qq.com>
2018-09-17 14:05:27 +0800

    Update and rename filter_fasta.py to filter_MetaGeneMark_by_size.py

--- filter_MetaGeneMark_by_size.py
+++ filter_MetaGeneMark_by_size.py
@@ -4,8 +4,8 @@ import argparse
 from Bio import SeqIO
 
 def get_args():
-	parser = argparse.ArgumentParser(description="用途：用于过滤fasta序列")
-	parser.add_argument("-i",dest="fastaFile",metavar="",help="输入文件：fasta格式")
+	parser = argparse.ArgumentParser(description="用途：根据序列长度过滤MetaGeneMark预测的genes序列，并根据文件名修改序列ID号")
+	parser.add_argument("-i",dest="fastaFile",metavar="",help="输入文件：MetaGeneMark预测的genes序列")
 	parser.add_argument("-l",dest="cutoff",metavar="",type=int,help="要保留的序列长度")
 	parser.add_argument("-o",dest="outFile",metavar="",help="输出文件名")
 	return parser.parse_args()
